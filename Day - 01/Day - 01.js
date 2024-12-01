84b8f7f11f3b049ea0ad0170b7755febb4fc00ba
MegaJerk <GarbageDonkey@gmail.com>
2024-12-01 02:31:19 -0500

    Update Indention of Comments To Spaces
    All indentation should now be done with Spaces instead of Tabs.

--- Day - 01/Day - 01.js
+++ Day - 01/Day - 01.js
@@ -26,13 +26,13 @@ var rightArray = [
 /////////////////////////////////////////////////
 ////// Day 01 - Problem 01 //////////////////////
 /////////////////////////////////////////////////
-//	pair up the numbers and measure how far apart they are.
-//	Pair up the smallest number in the left list with the smallest number
-//	in the right list, then the second-smallest left number with the
-//	second-smallest right number, and so on.
-//
-//	Within each pair, figure out how far apart the two numbers are;
-//	you'll need to add up all of those distances
+//  pair up the numbers and measure how far apart they are.
+//  Pair up the smallest number in the left list with the smallest number
+//  in the right list, then the second-smallest left number with the
+//  second-smallest right number, and so on.
+//  
+//  Within each pair, figure out how far apart the two numbers are;
+//  you'll need to add up all of those distances
 /////////////////////////////////////////////////
 
 /////////////////////////////////////////////////
@@ -53,10 +53,10 @@ console.log(totalDistance);
 /////////////////////////////////////////////////
 ////// Day 01 - Problem 02 //////////////////////
 /////////////////////////////////////////////////
-//	figure out exactly how often each number from the left list appears
-//	in the right list. Calculate a total similarity score by adding up
-//	each number in the left list after multiplying it by the number of
-//	times that number appears in the right list.
+//  figure out exactly how often each number from the left list appears
+//  in the right list. Calculate a total similarity score by adding up
+//  each number in the left list after multiplying it by the number of
+//  times that number appears in the right list.
 /////////////////////////////////////////////////
 
 /////////////////////////////////////////////////
